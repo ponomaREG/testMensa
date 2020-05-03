95e8c05926be8eebe63d5e576ec6e659843963ff
disfunction <ponomarcomru@gmail.com>
2020-05-03 19:38:29 +0600

    add string

--- app/src/main/java/com/example/mensatest/MensaResults.java
+++ app/src/main/java/com/example/mensatest/MensaResults.java
@@ -3,7 +3,6 @@ package com.example.mensatest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
